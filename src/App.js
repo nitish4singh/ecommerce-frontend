e0b66adaed69acf52b8a8d88d81a877d0242da5a
nitish4singh <nitishsingh4748@gmail.com>
2023-05-30 23:04:20 +0545

    homepage UI completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import {BrowserRouter , Routes ,Route} from "react-router-dom"
-import Layout from './components/Layout';
-import { Home } from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import { Home } from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import OurStore from "./pages/OurStore";
 function App() {
   return (
     <>
@@ -13,7 +14,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
-            </Route>
+            <Route path="store" element={<OurStore />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
