c61ca295aa79321dcb6b6f4e46986f522c0088ce
nitish4singh <nitishsingh4748@gmail.com>
2023-06-01 16:18:38 +0545

    some ui added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import { Home } from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
+import Blog from "./pages/Blog";
+import CompareProduct from "./pages/CompareProduct";
+import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Forgetpassword from "./pages/Forgetpassword";
+import Resetpassword from "./pages/Resetpassword";
+import SingleBlog from "./pages/SingleBlog";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ShippingPolicy from "./pages/ShippingPolicy";
+import TermAndContions from "./pages/TermAndCondition";
+import RefundPolicy from "./pages/RefundPolicy";
 function App() {
   return (
     <>
@@ -14,7 +26,19 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="blogs" element={<Blog />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
             <Route path="store" element={<OurStore />} />
+            <Route path="compare-product" element={<CompareProduct />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="forgot-password" element={<Forgetpassword />} />
+            <Route path="reset-password" element={<Resetpassword />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="refund-policy" element={<RefundPolicy />} />
+            <Route path="shipping-policy" element={<ShippingPolicy />} />
+            <Route path="term-conditions" element={<TermAndContions />} />
           </Route>
         </Routes>
       </BrowserRouter>
